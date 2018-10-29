ea766cba36ccf430af264fb92e51fce1b864932c
Jeff <jeffrey.lord@sap.com>
2018-10-29 15:28:07 -0400

    Added functionality for finding columns and rows
    More work needs to be done. This is a basic framework for how it could be accomplished. Non-table data will add extra columns, or may add random strings to existing columns if the text lines up.

---
+++ test.py
@@ -0,0 +1,20 @@
+import tabula
+import os
+
+fname = "pdf6"
+pdfname = fname + ".pdf"
+csvname = fname + ".csv"
+pandas_options = {'header': None}
+df = tabula.read_pdf(pdfname, multiple_tables=False,
+                     guess=True, pandas_options=pandas_options)
+
+toDelete = []
+# for index, row in df.iterrows():
+#     print(row)
+print(df)
+
+try:
+    os.remove(csvname)
+except OSError:
+    pass
+tabula.convert_into(pdfname, csvname, output_format="csv")
